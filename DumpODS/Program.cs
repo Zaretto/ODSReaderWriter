ddcec6c756604bccc2104c1341df171289f1cfc7
Richard Harrison <rjh@zaretto.com>
2022-12-11 22:08:32 +0000

    Basline version

---
+++ DumpODS/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Data;
+using System.Linq;
+using System.Text;
+using Zaretto.ODS;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var odsReaderWriter = new ODSReaderWriter();
+var spreadsheetData = odsReaderWriter.ReadOdsFile(@"c:\temp\ods-test.ods");
+if (spreadsheetData != null)
+{
+    foreach (DataTable table in spreadsheetData.Tables)
+    {
+        System.Console.WriteLine("Sheet {0}", table.TableName);
+        foreach (var row in table.AsEnumerable())
+        {
+            if (row.ItemArray.Any(ia => !string.IsNullOrEmpty(ia.ToString())))
+                System.Console.WriteLine("    {0}", string.Join(",", row.ItemArray.Select(xx => xx.ToString())));
+        }
+    }
+    odsReaderWriter.WriteOdsFile(spreadsheetData, "c:/temp/test.ods");
+}
---
+++ OdsReaderWriter/README.md
@@ -0,0 +1,20 @@
+# C# Open Document Spreadsheet Reader Writer
+
+This is a simple ODS reader writer. 
+
+ref: https://opendocumentformat.org/
+
+Based on the work of Josip Kremenic[1]
+
+## NOTES
+
+Tested only with reading LibreOffice.
+
+# Developers
+
+* Richard Harrison
+* Nathan Crawford (@njcrawford )
+
+----
+[1] https://www.codeproject.com/Articles/38425/How-to-Read-and-Write-ODF-ODS-Files-OpenDocument-2
+
